52fbf8cfc24f41e237cf57cefe40b3ba88463b45
karla nava <karla.nm8@gmail.com>
2017-03-29 11:47:04 +0000

    Agrega lista nueva pero como undefined

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@ function agregarPendientes() {
   var pendientes = document.getElementById("pendientePorAgregar").value;
   var li = document.createElement("li"); 
   
-  li.innerHTML = pendientes == ''?'(pendiete sin crar)':pendientes;
+  li.innerHTML = pendientes == ''?'(pendiete sin crear)':pendientes;
   document.getElementById('listaPendientes').appendChild(li);
 }
 
@@ -13,10 +13,22 @@ function borrarPendientes() {
   listaPendientes.removeChild(elemento[i]);
 }
 
-function crearListaPendientes() {
-  var pendientes = document.getElementById("pendientePorAgregar").value;
-  var li = document.createElement("li"); 
-  
-  li.innerHTML = pendientes == ''?'(pendiete sin crar)':pendientes;
-  document.getElementById('listaPendientes').appendChild(li);
+
+function agregarListaPendientes() {
+	var listaGeneralPendientes = document.getElementById("contieneLista").value;
+	var section = document.createElement("section");
+
+	section.innerHTML = listaGeneralPendientes == ''?'(lista general pendiete sin crear)':listaGeneralPendientes;
+	document.getElementById("contieneLista").appendChild(section);
+
+
 }
+
+/* 	var listaAClonar = document.getElementById("contieneLista").lastChild;
+	var clone = listaAClonar.cloneNode(true);
+	var destino = document.getElementById("contieneLista");
+	destino.appendChild(clone);
+	console.log(agregarListaPendientes());
+*/
+
+
