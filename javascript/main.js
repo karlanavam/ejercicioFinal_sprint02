59fdeb7e428e08e90b74938138fbd73400506dc3
karla nava <karla.nm8@gmail.com>
2017-03-29 13:55:42 +0000

    Agregué otra versión con constructor, no funcionó aún

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,41 @@
+/*function ListaPendientes (titulo) {
+	this.titulo = titulo;
+	this.pendientes = pendientes; 
+	this.agregar = agregar;
+	this.borrar = borrar;
+	
+}
+
+var listas = [];
+
+function agregarPendientes() {
+	var pendientes = document.getElementById("pendientePorAgregar").value;
+	var lista = document.createElement("li");
+	var textoLista = document.createTextNode(pendientes);
+	lista.appendChild(textoLista);
+	document.getElementById("listaPendientes").appendChild(lista);
+}
+
+function borrarPendientes() {
+  var pendientesBorrar = document.getElementById("listaPendientes");
+  var elemento = document.getElementsByTagName("li");
+  var i = elemento.length -1;
+  listaPendientes.removeChild(elemento[i]);
+}
+
+function crearListaNueva() {
+ var crearListaSecundaria = document.getElementById("contieneLista").value; 
+ var lista = document.createElement("section"); 
+ var textoLista = document.createTextNode(listaPorCrear.value); 
+   lista.appendChild(textoLista);
+   document.getElementById("contieneLista").appendChild(lista); 
+   
+}
+
+
+var listaUno = new ListaPendientes ("Pendientes semana;")*/
+
+
 function agregarPendientes() {
 	var pendientes = document.getElementById("pendientePorAgregar").value;
 	var lista = document.createElement("li");
